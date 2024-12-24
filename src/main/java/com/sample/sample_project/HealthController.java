30809a43c84465cdddb892bbe4f2780420fc2041
shubham <shubhamsingh123987@gmail.com>
2024-12-25 00:18:30 +0530

    health api done

---
+++ src/main/java/com/sample/sample_project/HealthController.java
@@ -0,0 +1,19 @@
+package com.sample.sample_project;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthController {
+
+    @GetMapping
+    public ResponseEntity<String> getHealthStatus() {
+
+        return ResponseEntity.ok("Health okay tata bye");
+    }
+}
